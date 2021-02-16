38d44fd09c7b0d64144815a36a67c326fb6b4e68
taweewatmail <taweewatmail@gmail.com>
2021-02-16 22:40:52 +0700

    commit all changes

--- client/src/api/aboutmeindex.js
+++ client/src/api/aboutmeindex.js
@@ -3,6 +3,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000/apiaboutme",
+    baseURL: "http://localhost:3001/apiaboutme",
     responseType: "json"
 });
