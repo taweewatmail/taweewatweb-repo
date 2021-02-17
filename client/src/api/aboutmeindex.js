025b24514d346131c5bc8f64c4933ce14a276533
taweewatmail <taweewatmail@gmail.com>
2021-02-17 19:10:51 +0700

    update all

--- client/src/api/aboutmeindex.js
+++ client/src/api/aboutmeindex.js
@@ -3,6 +3,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/apiaboutme",
+    baseURL: "https://taweewat.herokuapp.com/apiaboutme",
     responseType: "json"
 });
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://taweewat.herokuapp.com/api',
 })
 
 export const insertJoke = payload => api.post(`/joke`, payload)
