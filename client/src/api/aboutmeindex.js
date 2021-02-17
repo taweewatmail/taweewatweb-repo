21eb9f96ad70fbd2954e67fdb22ae8e794764495
taweewatmail <taweewatmail@gmail.com>
2021-02-17 11:57:26 +0700

    update all

--- client/src/api/aboutmeindex.js
+++ client/src/api/aboutmeindex.js
@@ -3,6 +3,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://limitless-falls-29739.herokuapp.com:3001/apiaboutme",
+    baseURL: "https://taweewat.herokuapp.com:3001/apiaboutme",
     responseType: "json"
 });
